0dba97acfcf26712c16ced7d4519a4db1711fb7e
pranjali6 <itspranjali06@gmail.com>
2023-07-30 11:19:45 +0530

    single post

---
+++ src/Pages/home/Homepge.jsx
@@ -0,0 +1,17 @@
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./homepge.css";
+
+export default function Homepge() {
+
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
